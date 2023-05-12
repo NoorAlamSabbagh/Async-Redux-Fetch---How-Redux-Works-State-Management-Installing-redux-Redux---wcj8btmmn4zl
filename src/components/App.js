9adad255e0070fd885aa8577cb9495ae8c887d20
Noor Alam <99783083+NoorAlamSabbagh@users.noreply.github.com>
2023-05-12 13:01:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,29 @@ import "../styles/App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { actions } from "./store";
 
-const API_KEY = "f987415313da4ae481971fcb5c94772a"; //Get your own api key from newsapi
+const API_KEY = "068e2971b1d74bfc96fa703c112fc5be"; //Get your own api key from newsapi
 
 const App = () => {
-  const [newsCount, setNewsCount] = useState(1);
   const dispatch = useDispatch();
-
-  //useSelector allow use to access the state of store
-  const newsObj = useSelector((state) => state.news);
-
-  //fetching data and dispatching result to store
+  const newsObj = useSelector((state) => state.hotNews);
   useEffect(() => {
-    async function fetchdata() {
-      const data = await fetch(
-        ` https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&pageSize=${newsCount}`
-      );
-      const res = await data.json();
-      dispatch(actions.setnews(res.articles));
-      return res;
-    }
-    fetchdata();
-  }, [newsCount]);
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+      .then((json) => json.json())
+      .then((obj) => {
+        dispatch(actions.set({ articles: obj.articles, num: 5 }));
+      });
+  }, []);
 
   const numChangeHandler = (e) => {
-    if (e.target.value !== "") setNewsCount(e.target.value);
+    dispatch(actions.setNum({ num: e.target.value }));
   };
 
   let articles = [...newsObj.articles];
 
-  const filteredArticles = articles;
+  const filteredArticles = articles
+    .sort(() => Math.random() - 0.5)
+    .slice(0, newsObj.articlesNum);
+  console.log(filteredArticles);
 
   return (
     <div id="main">
@@ -49,9 +43,9 @@ const App = () => {
         <div>
           <h3>Top {newsObj.articlesNum} articles</h3>
           <ul id="articles">
-            {filteredArticles.map((item, index) => {
+            {filteredArticles.map((item) => {
               return (
-                <li key={index}>
+                <li>
                   <div className="article">
                     Author: {item.author}
                     <h2>{item.title}</h2>
